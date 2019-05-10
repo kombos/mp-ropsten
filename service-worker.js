d701e9195870b7e0a20acbd6737a00f0af6344ef
kombos <18464450+kombos@users.noreply.github.com>
2019-05-10 21:51:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebfbe9288195e52cea178686c7947677.js"
+  "/precache-manifest.24ee08bf1bf4c328229db5ccc481c0ae.js"
 );
 
 self.addEventListener('message', (event) => {
